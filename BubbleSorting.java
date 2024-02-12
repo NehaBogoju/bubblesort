6fafa2f799f658913eb9367ef26395858271d0f9
Neha3010 <158296501+NehaBogoju@users.noreply.github.com>
2024-02-12 22:57:08 +0530

    Add files via upload

---
+++ BubbleSorting.java
@@ -0,0 +1,24 @@
+package DSA;
+
+public class BubbleSorting {
+
+	public static void main(String[] args) {
+		int arr[]= {64, 34, 25, 12, 22, 11, 90};
+		for(int i=0;i<arr.length;i++) 
+		{
+			for(int j=0;j<arr.length-1;j++)
+			{
+				if(arr[j]>arr[j+1]) 
+				{
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		for(int i=0;i<arr.length;i++)
+			System.out.print(arr[i]+" ");
+
+	}
+
+}
